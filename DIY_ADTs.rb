6f22914b0dbff1d98062397e84b7ac5ca9627dab
Enrique Folte <hhfolte@gmail.com>
2022-02-08 19:56:18 -0500

    Add stack, queue and map classes

--- DIY_ADTs.rb
+++ DIY_ADTs.rb
@@ -45,5 +45,28 @@ class Stack
     private
 
     attr_reader :underlying_array
+  end
 
+  class Map
+    def initialize
+        underlying_array = []
+    end
+
+    def set(key, value)
+        pair_index = underlying_array.index { |pair| pair[0] = key }
+        if pair_index
+            underlying_array[pair_index][1] = value
+        else
+            underlying_array.push([key, value])
+        end
+        value
+    end
+
+    def get(key)
+        underlying_array.each { |pair| return pair[1] if pair[0] == key } 
+    end
+
+    def delete(key)
+        underlying_array.reject! { |pair| pair[0] == key ] }
+    end
   end
